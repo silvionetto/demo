6b6d27fc025232237c87fbff2687a259714baa69
Silvio Netto <silvio.netto@gmail.com>
2025-10-23 21:46:41 +0200

    Initial layout

---
+++ src/main/java/com/silvionetto/demo/DemoApplicationController.java
@@ -0,0 +1,12 @@
+package com.silvionetto.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoApplicationController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
