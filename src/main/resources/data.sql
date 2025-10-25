4c5d220cf8797bd7c467c89397a27ca2d25b4fac
Silvio Netto <silvio.netto@gmail.com>
2025-10-25 12:14:32 +0200

    Adding data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO demo_role (id, name, description, version, last_update_date) VALUES (1, 'ADMIN', 'Administrator',0, NOW());
+INSERT INTO demo_role (id, name, description, version, last_update_date) VALUES (2, 'USER', 'User',0, NOW());
